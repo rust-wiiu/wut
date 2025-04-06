4d21584cc22813f133e4929ccad1e13b83d6cea5
29th-Day <43274844+29th-Day@users.noreply.github.com>
2025-04-06 02:23:33 +0200

    refactor

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -7,10 +7,9 @@ mod once_lock;
 mod rwlock;
 mod semaphore;
 
+pub use event::{AutoEvent, ManualEvent};
 pub use lazy_lock::LazyLock;
 pub use mutex::{Mutex, MutexError};
 pub use once_lock::OnceLock;
 pub use rwlock::RwLock;
-
-pub use event::{AutoEvent, ManualEvent};
 pub use semaphore::Semaphore;
